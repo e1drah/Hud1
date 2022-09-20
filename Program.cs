78c675b6455230d5de8acd46de0884646ae2da23
e1drah <113189459+e1drah@users.noreply.github.com>
2022-09-20 10:44:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,56 @@ namespace Hud1
     {
         static void Main(string[] args)
         {
+            //variables int
+            int health;
+            int score;
+            int lives;
+            int attackPower;
+            int monsterKillPoints;
+            int locationFoundPoints;
+            int itemFoundPoints;
+            int monsterStongPower;
+            int monsterWeakPower;
+            int monsterMPower;
+            //variables string
+            string playerName;
+            //initialization
+            health = 100;
+            score = 0;
+            lives = 3;
+            attackPower = 10;
+            monsterKillPoints = 100;
+            locationFoundPoints = 50;
+            monsterStongPower = 10;
+            monsterWeakPower = 10;
+            monsterMPower = 
+
+            Console.WriteLine("Enter name");
+            playerName = Console.ReadLine();
+            Console.WriteLine(" Your name is " + playerName + ".");
+            Console.WriteLine("Press ANY KEY to continue ");
+            Console.ReadKey();
+            Console.Clear();
+            //Actual "game"
+            Console.WriteLine(playerName + ": Health: " + health + " Lives: " + lives + " score: " + score);
+            Console.WriteLine();
+            Console.WriteLine(" O");
+            Console.WriteLine("/|7");
+            Console.WriteLine(" /7");
+            Console.WriteLine();
+            Console.WriteLine(playerName + " 's adventure begins!");
+            Console.ReadKey(true);
+            Console.Clear();
+            // player finds a abandoned house
+            score += 50;
+            Console.WriteLine(playerName + ": Health: " + health + " Lives: " + lives + " score: " + score + " Attack Power: ");
+            Console.WriteLine();
+            Console.WriteLine(" O" +  "MMMM  ");
+            Console.WriteLine("/|7" + "|  | ");
+            Console.WriteLine(" /7" + "|[]|");
+            Console.WriteLine();
+            Console.WriteLine(playerName + " has found an abandoned house");
+            
         }
     }
 }
