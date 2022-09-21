d409ccab3cd4bfb72107bf77d280a4bc8b8b4ce1
e1drah <113189459+e1drah@users.noreply.github.com>
2022-09-21 10:30:23 -0300

    Added Monster

--- Program.cs
+++ Program.cs
@@ -11,38 +11,61 @@ namespace Hud1
         static void Main(string[] args)
         {
             //variables int
+            //Player stats
             int health;
             int score;
             int lives;
             int attackPower;
+            int scoreMultipier;
+
+            //Monster stars
             int monsterKillPoints;
+
+            int monsterStongPower;
+            int monsterStronghealth;
+
+            int monsterWeakPower;
+            int monsterWeakHealth;
+
+            int monsterMediumPower;
+            int monsterMediumHealth;
+
             int locationFoundPoints;
             int itemFoundPoints;
-            int monsterStongPower;
-            int monsterWeakPower;
-            int monsterMPower;
+
             //variables string
             string playerName;
+
             //initialization
             health = 100;
             score = 0;
             lives = 3;
             attackPower = 10;
-            monsterKillPoints = 100;
+            scoreMultipier = 1;
+            
             locationFoundPoints = 50;
+            monsterKillPoints = 100;
+            itemFoundPoints = 75;
+
             monsterStongPower = 50;
             monsterWeakPower = 10;
-            monsterMPower = 25;
+            monsterMediumPower = 25;
+
+            monsterWeakHealth = 25;
+            monsterMediumHealth = 50;
+            monsterStronghealth = 100;
+
             Console.WriteLine(" O");
             Console.WriteLine("/|7");
             Console.WriteLine(" /7");
             Console.WriteLine();
-            Console.WriteLine("Enter name");
+            Console.WriteLine("Enter their name");
             playerName = Console.ReadLine();
             Console.WriteLine(" Your name is " + playerName + ".");
             Console.WriteLine("Press ANY KEY to continue ");
             Console.ReadKey();
             Console.Clear();
+
             //Actual "game"
             Console.WriteLine(playerName + ": Health: " + health + " Lives: " + lives + " score: " + score);
             Console.WriteLine();
@@ -50,19 +73,35 @@ namespace Hud1
             Console.WriteLine("/|7");
             Console.WriteLine(" /7");
             Console.WriteLine();
-            Console.WriteLine(playerName + " 's adventure begins!");
+            Console.WriteLine(playerName + "'s adventure begins!");
             Console.ReadKey(true);
             Console.Clear();
+
             // player finds a abandoned house
             score += 50;
             Console.WriteLine(playerName + ": Health: " + health + " Lives: " + lives + " score: " + score + " Attack Power: ");
             Console.WriteLine();
-            Console.WriteLine(" O" +  "MMMM  ");
-            Console.WriteLine("/|7" + "|  | ");
-            Console.WriteLine(" /7" + "|[]|");
+            Console.WriteLine("    " +   "  ____");
+            Console.WriteLine(" O  " +  @" /____\");
+            Console.WriteLine("/|7 " +   " | __ |");
+            Console.WriteLine(" /7 " +   " |_||_| ");
             Console.WriteLine();
             Console.WriteLine(playerName + " has found an abandoned house");
-            
+            Console.ReadKey(true);
+            Console.Clear();
+
+            // player finds a monster
+            Console.WriteLine(playerName + ": Health: " + health + " Lives: " + lives + " score: " + score + " Attack Power: " + attackPower);
+            Console.WriteLine("Monster Health: " + monsterWeakHealth + " Attack Power: " + monsterWeakPower);
+            Console.WriteLine();
+            Console.WriteLine("    " +  " {}");
+            Console.WriteLine(" O  " + @"/()\");
+            Console.WriteLine("/|7 " +  " []");
+            Console.WriteLine(" /7 " +  " ||");
+            Console.WriteLine();
+            Console.WriteLine(playerName + " has found a WEAK Monster!!");
+            Console.ReadKey(true);
+
         }
     }
 }
